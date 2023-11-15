0d09d8bbd557de782bace593a86ab847b772cbc8
Tyutyunin Nikolai <113365948+white-veld@users.noreply.github.com>
2023-11-16 06:04:05 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,27 @@
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot('...')
+bot = telebot.TeleBot('token')
+
+if True:
+    @bot.message_handler(commands=['start'])
+    def start(message):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        create_account = types.KeyboardButton('Создать профиль')
+        markup.add(create_account)
+        bot.send_message(message.chat.id, 'Я бот FoF. Здесь вы можете найти с кем подраться или потрахаться', reply_markup=markup)
+        id = message.from_user.id
+        bot.register_next_step_handler(message, name)
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    create_account = types.KeyboardButton('Создать профиль')
-    markup.add(create_account)
-    bot.send_message(message.chat.id, 'Я бот FoF. Здесь вы можете найти с кем подраться или потрахаться', reply_markup=markup)
-
-
-
-@bot.message_handler(content_types=['text'])
-def text(message):
-    if message.text == 'Создать профиль':
+    def name(message):
         bot.send_message(message.chat.id, 'Напиши имя')
+        bot.register_next_step_handler(message, age)
+
+
+    def age(message):
+        bot.send_message(message.chat.id, 'Сколько тебе лет?')
+        bot.register_next_step_handler(message)
 
 
 @bot.message_handler(content_types=['photo'])
