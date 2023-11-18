e65acd2d88a5955c14301a068f782bdcf04a5f84
Evolvento <137023249+Evolvento@users.noreply.github.com>
2023-11-18 20:45:15 +0800

    delete menu.py

--- menu.py
+++ menu.py
@@ -1,14 +1,1 @@
-from profile import Profile
-from controller import Controller
-from strategy_love import StrategyLove
-from strategy_fight import StrategyFight
 
-
-class Menu:
-    def __init__(self):
-        self.array_profiles = {}
-
-
-
-    def display_profile(self, profile):
-        pass
